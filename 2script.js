cb3dd0ec13fb941aed31ecfad1aa9a5dd4a14a1d
Anmol Kumar <at4141anmol@gmail.com>
2026-04-07 22:09:09 +0530

    try new version for Imp and Expo

--- 2script.js
+++ 2script.js
@@ -1,4 +1,11 @@
 var a = 78;
-console.log(a);
  
-module.exports = a;
+
+console.log("This is the second script");
+console.log("This is export successfully");
+console.log("code is running successfully");
+
+// module.exports = a;
+
+export default a;
+
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-require
+// var data = require('./2script.js');       old version of js
+// console.log(data);
+
+import data from './2script.js';            // new version of js
+console.log(data); 
