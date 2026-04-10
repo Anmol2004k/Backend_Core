5831509aa8ae7a70e5884683da845650a1e929f0
Anmol Kumar <at4141anmol@gmail.com>
2026-04-10 19:59:06 +0530

    using  1 liner joke package

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ browser,V8 Engine, overall working
 using import , export
 use npm
 express
+
+uses of any packeage 
+1. npm init -y   >>>>     es s setup hoga envirioment node  module , package.json etc..
+2. npm i package ka naam >>> wo package install ho jygaa
+3. 
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // var data = require('./2script.js');       old version of js
 // console.log(data);
 
-import data from './2script.js';            // new version of js
-console.log(data); 
+// import data from './2script.js';            // new version of js
+// console.log(data); 
+
+// using npm package
+
+var oneLinerJoke = require('one-liner-joke');
+console.log( oneLinerJoke.getRandomJoke());
